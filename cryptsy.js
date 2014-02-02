ac8de2889f54b96b9a8879c05d9477e9b949f57f
Daniil Pokrovksy <danii.pokrovsky@gmail.com>
2014-02-02 13:10:18 +0400

    Updated API url

--- cryptsy.js
+++ cryptsy.js
@@ -18,7 +18,7 @@ function CryptsyClient(key, secret) {
     args = args_tmp;
 
     var options = {
-      uri: 'https://www.cryptsy.com/api',
+      uri: 'https://api.cryptsy.com/api',
       agent: false,
       method: 'POST',
       headers: {
